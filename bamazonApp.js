cfa857951e93589bc9387f0f4e879332e55c9180
Vaniri <foxifennec@gmail.com>
2020-01-26 17:14:30 -0500

    added handle purchase function

--- bamazonApp.js
+++ bamazonApp.js
@@ -7,28 +7,52 @@ const db = mysql.createConnection({
     user: "",
     password: "",
     database: "bamazon_db"
-  });
+});
 
-  db.connect();
-
-  db.query("SELECT * FROM products", (err, choices) => {
+db.connect(err => {
     if (err) throw err;
-    runUnquirer(choices);
-
   });
 
-  function runUnquirer(choices){
-  inquirer 
-  .prompt ({
-      name: "choice",
-      type: "list",
-      message: "Welcome to the Bamazon! Check your amazing items and buy what you like!",
-      choices: choices.map(item => { return {
-          name: `${item.product_name} price: ${item.price}`,
-          value: item.id
-      }})
-  })
-  .then(answer => {
-    console.log(answer.choice);
-  })
+db.query("SELECT * FROM products", (err, choices) => {
+    if (err) throw err;
+    runInquirer(choices);
+
+});
+
+function runInquirer(choices) {
+    inquirer
+        .prompt({
+            name: "choice",
+            type: "list",
+            message: "Welcome to the Bamazon! Check your amazing items and buy what you like!",
+            choices: choices.map(item => {
+                return {
+                    name: `${item.product_name} price: ${item.price}`,
+                    value: item.id
+                }
+            })
+        })
+        .then(answer => {
+            handleProduct(answer.choice);
+        })
 }
+
+function handleProduct (itemId) {
+    inquirer
+        .prompt({
+            name: "quantity",
+            type: "input",
+            message: "How many units would you like to buy?",
+        })
+        .then(answer => {
+            console.log(answer.quantity);
+            db.query(
+                "UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?", 
+                [ answer.quantity, itemId ],
+                err => {
+                    if (err) throw err;
+                    console.log("Thank you for your purchase!");
+                }
+                )
+        })
+    }
