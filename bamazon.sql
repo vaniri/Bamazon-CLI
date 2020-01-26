dcbe03dc8e00118d368469f8646608c9b602137e
Vaniri <foxifennec@gmail.com>
2020-01-26 14:59:05 -0500

    added products database

---
+++ bamazon.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+    id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(50) NOT NULL,
+    price DECIMAL (10,2) NOT NULL,
+    stock_quantity INT default 0 NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity )
+VALUES ("Justin Timberlake's half-eaten French toast", "grocery", 1000.03, 1),
+       ("Broken laser pointe", "necessity", 14.38, 1),
+       ("Chost in a jar", "necessity", 31.01, 1),
+       ("gummy bear", "grocery", 3.99, 50),
+       ("Eloquent JavaScript", "books", 25.49, 10),
+       ("Introduction to Algorithms", "books", 63.37, 20),
+       ("Cards Against Humanity", "games", 25.91, 12),
+       ("Milk-Bone MaroSnacks Dog Treats", "pets", 6.79, 100),
+       ("Friends Forever Donut Cat Bed", "pets", 39.99, 5),
+       ("Endless coffee mug", "necessity", 24.99, 3);
+
+
+
+
+
+
+
+
+
