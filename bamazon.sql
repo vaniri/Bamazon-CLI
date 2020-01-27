aedf02e10d4eb54ae3aa80e79d7665b325198bfa
Vaniri <foxifennec@gmail.com>
2020-01-27 15:39:49 -0500

    added function to handle new purchase or exit

--- bamazon.sql
+++ bamazon.sql
@@ -8,7 +8,7 @@ CREATE TABLE products (
     product_name VARCHAR(100) NOT NULL,
     department_name VARCHAR(50) NOT NULL,
     price DECIMAL (10,2) NOT NULL,
-    stock_quantity INT default 0 NOT NULL,
+    stock_quantity INT CHECK (stock_quantity >= 0) default 0 NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -16,7 +16,7 @@ INSERT INTO products (product_name, department_name, price, stock_quantity )
 VALUES ("Justin Timberlake's half-eaten French toast", "grocery", 1000.03, 1),
        ("Broken laser pointe", "necessity", 14.38, 1),
        ("Chost in a jar", "necessity", 31.01, 1),
-       ("gummy bear", "grocery", 3.99, 50),
+       ("Gummy bear", "grocery", 3.99, 50),
        ("Eloquent JavaScript", "books", 25.49, 10),
        ("Introduction to Algorithms", "books", 63.37, 20),
        ("Cards Against Humanity", "games", 25.91, 12),
--- bamazonApp.js
+++ bamazonApp.js
@@ -1,6 +1,12 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+const price = '\033[0;34m';
+const outOfStock = '\033[0;31m';
+const exit = '\033[0;36m';
+const reset = '\033[0m';
+const divider = '-------------------------------------------------------------------------------------'
+
 const db = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -11,33 +17,37 @@ const db = mysql.createConnection({
 
 db.connect(err => {
     if (err) throw err;
-  });
-
-db.query("SELECT * FROM products", (err, choices) => {
-    if (err) throw err;
-    runInquirer(choices);
-
 });
+
+function startShopping() {
+    db.query("SELECT * FROM products", (err, choices) => {
+        if (err) throw err;
+        runInquirer(choices);
+
+    });
+}
+
+startShopping();
 
 function runInquirer(choices) {
     inquirer
         .prompt({
             name: "choice",
             type: "list",
-            message: "Welcome to the Bamazon! Check your amazing items and buy what you like!",
+            message: `Welcome to the Bamazon! Check our amazing items and buy what you like!\n`,
             choices: choices.map(item => {
                 return {
-                    name: `${item.product_name} price: ${item.price}`,
-                    value: item.id
+                    name: `* ${item.product_name} ${price}$$${item.price} ${reset}`,
+                    value: item
                 }
             })
         })
         .then(answer => {
-            handleProduct(answer.choice);
+            handleProduct(answer.choice.id, answer.choice.price);
         })
 }
 
-function handleProduct (itemId) {
+function handleProduct(itemId, itemPrice) {
     inquirer
         .prompt({
             name: "quantity",
@@ -45,14 +55,39 @@ function handleProduct (itemId) {
             message: "How many units would you like to buy?",
         })
         .then(answer => {
-            console.log(answer.quantity);
             db.query(
-                "UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?", 
-                [ answer.quantity, itemId ],
+                "UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?",
+                [answer.quantity, itemId],
                 err => {
-                    if (err) throw err;
-                    console.log("Thank you for your purchase!");
+                    if (err) {
+                        console.log(`${outOfStock} Sorry! The item is out of stock! ${reset}`);
+                        newPurchase();
+                    } else {
+                        let totalPrice = itemPrice * answer.quantity;
+                        console.log(`Total price: ${price} ${totalPrice} ${reset}`);
+                        console.log("Thank you for your purchase!");
+                        newPurchase();
+                    }
                 }
-                )
+            )
         })
-    }
+}
+
+function newPurchase() {
+    inquirer
+        .prompt({
+            name: "answer",
+            type: "list",
+            message: " ",
+            choices: ["BACK TO THE STORE", "EXIT"]
+        })
+        .then(answer => {
+            if (answer.answer === "EXIT") {
+                db.destroy();
+            } else {
+                startShopping();
+            }
+        });
+
+}
+
